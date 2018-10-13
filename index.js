4b41f81b6a643107158a68e3cfda3fe0b0ca049d
Ravi Kumar <ravi.kumar@hammerspace.com>
2018-10-14 03:22:21 +0530

    server changes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express');
 const path = require('path')
-var app = express();
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
 app.use(express.static(path.join(__dirname, './')));
-// NODE_ENV === 'production'
-const PORT = process.env.NODE_ENV || 9000
 
 app.listen(PORT, ()=> {
     console.log('Express server is up on port ' + PORT);
